89e6d4ecd5a793c183a88e69516f3fdce89c23c5
Min Jung Park <mp3369@columbia.edu>
2016-02-13 17:36:40 -0500

    level 3 completed for API

--- flask/books.py
+++ flask/books.py
@@ -2,5 +2,6 @@ import requests
 
 url = "https://www.googleapis.com/books/v1/volumes?q=Treasure%20Island"
 response = requests.get(url)
+response_dict = response.json()
 
-print response
+print response_dict
